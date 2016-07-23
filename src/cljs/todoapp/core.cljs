d8d3b52c85c905a0ead5371a23683bff56ca0a24
Aleksey Lovchikov <svdmusic@gmail.com>
2016-07-23 20:50:48 +0300

    delete todos

--- src/cljs/todoapp/core.cljs
+++ src/cljs/todoapp/core.cljs
@@ -1,53 +1,67 @@
 (ns todoapp.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
-(defonce app-state (atom {:todos [
-    {:desc "Buy Milk" :id 0}
-    {:desc "Go to Shop" :id 1}
-    {:desc "Buy Car" :id 2}
-    {:desc "Learn Clojure" :id 3}
-    {:desc "Feed Cat" :id 4}
-    {:desc "Drink Vodka" :id 5}
-  ]
-}))
+(defonce app-state
+  (atom
+    {:todos
+     [{:desc "Go to Shop and buy milk 3.2%"} {:desc "Learn Clojure for job"}]}))
 
-(defn form [text owner]
+(defn todo-view [todo owner]
   (reify
-    om/IRender
-    (render [_]
-      (dom/form nil (dom/input nil)))))
+    om/IRenderState
+    (render-state [this {:keys [delete]}]
+      (dom/li nil
+        (dom/span nil (:desc todo))
+        (dom/button #js {:onClick (fn [e] (put! delete @todo))} "Delete")))))
 
-(defn todos []
-  (om/ref-cursor (:todos (om/root-cursor app-state))))
+(defn add-todo [data owner]
+  (let [new-todo (-> (om/get-node owner "todo-from-input")
+                        .-value
+                        :todos data)]
+                        (println new-todo)
+    (when new-todo
+      (om/transact! data :todos #(conj % new-todo))
+      (om/set-state! owner :desc ""))))
 
-(defn todo-item-view [todo owner]
+(defn handle-change [e owner {:keys [text]}]
+  (let [value (.. e -target -value)]
+    (if-not (re-find #"[0-9]" value)
+      (om/set-state! owner :desc value)
+      (om/set-state! owner :desc text))))
+
+(defn todos-view [data owner]
   (reify
-    om/IRender
-    (render [_]
-      (dom/div nil (dom/strong nil (:desc todo))
-        (dom/p nil (dom/button #js {:onClick (fn [e] (let [cs (todos)] (om/update! cs (vec (remove #(= (:id todo) (:id %)) cs)))))} "delete"))))))
-
-(defn todos-view [todos owner]
-  (reify
-    om/IRender
-    (render [_]
-        (dom/div nil
-          (dom/h1 nil "ToDo List")
-          (doall (om/build-all todo-item-view todos))))))
-
-(defn app-view [state owner]
-  (reify
-    om/IRender
-    (render [_]
+    om/IInitState
+    (init-state [_]
+      {:delete (chan)
+       :desc ""})
+    om/IWillMount
+    (will-mount [_]
+      (let [delete (om/get-state owner :delete)]
+        (go (loop []
+              (let [todo (<! delete)]
+                (om/transact! data :todos
+                  (fn [xs] (vec (remove #(= todo %) xs))))
+                (recur))))))
+    om/IRenderState
+    (render-state [this state]
       (dom/div nil
-        (om/build form ())
-        (om/build todos-view (:todos state))
-        ))))
+        (apply dom/ul nil
+          (om/build-all todo-view (:todos data)
+            {:init-state state}))
+        (dom/div nil
+          (dom/input
+            #js {:type "text" :ref "todo-from-input" :value (:desc state)
+                 :onChange #(handle-change % owner state)})
+          (dom/button #js {:onClick #(add-todo data owner)} "Add"))))))
 
 (om/root
- app-view
- app-state
- {:target (js/document.getElementById "app")})
+  todos-view
+  app-state
+  {:target (. js/document (getElementById "app"))})
